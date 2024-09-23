4bf24b9d536edc021f17a57439e5e1035b1cfdd8
badah <ohb234@newschool.edu>
2024-09-23 16:05:38 -0400

    adding

--- unit-1/lesson-3/assignment-3-2.py
+++ unit-1/lesson-3/assignment-3-2.py
@@ -2,11 +2,19 @@ import sys
 from PIL import Image
 
 if len(sys.argv) != 3:
+    #! is to say opposite 
+    # could change to 4 
     exit("This command requires two arguments")
 
 img1 = Image.open( sys.argv[1] )
 img2 = Image.open( sys.argv[2] )
+# img3 = Image.open(sys.argv[3])
 
+#three_img_blend = Image.blend(blended_img, img3, .2) 
+#three_img_blend.save("blend3.jpg") 
+# on terminal: python3 assignment3-2.py Image-1.jpg , image-1-B.jpg, image-1-A.jpg ?
 blended_img = Image.blend(img1,img2,.5)
 blended_img.save("blended.jpg")
-
+# make sure images match up in size 
+# only takes 3 arguments at a time 
+# argv 1 being first image argc 2 being second image and third being the opacity 
