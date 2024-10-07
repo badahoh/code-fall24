a9042541f5da9dcc3b2299acfd77a435961f205d
badah <ohb234@newschool.edu>
2024-10-07 16:27:07 -0400

    all

--- unit-1/final-project/algo1.py
+++ unit-1/final-project/algo1.py
@@ -17,10 +17,36 @@ random_file = random.choice(files)
 random_file2 = random.choice(files)
 print(random_file, random_file2)
 #choosing random image from folder 
-img = Image.open( path.join(sys.argv[1],random_file) ) 
- 
-  
+randomimage = Image.open( path.join(sys.argv[1],random_file) ) 
+randomimage.thumbnail((400,400))
+#thumbnail instead of resize 
+randomimage2 = Image.open( path.join(sys.argv[1],random_file2) ) 
+randomimage2.thumbnail((400,400))
+# look at lesson4 excersise 6 
+canvas = Image.new("RGB", (800,800), "white")
+# need A in RGBA for transparency 
+canvas.paste(randomimage,(0,0))
+canvas.save("canvas.jpg")
 
+# if len(sys.argv) != 3:
+#     #! is to say opposite 
+#    #could change to 4 
+#     #exit("This command requires two arguments")
+
+# random_file = Image.open( sys.argv[1] )
+# random_file2 = Image.open( sys.argv[2] )
+# img = Image.open(sys.argv[3])
+# # (img is red stripes)
+
+# two_img_blend = Image.blend(random_file, random_file2, .3) 
+# two_img_blend.save("blend2random.jpg") 
+## on terminal: python3 algo1.py.py (can enter shells or sea instead of image files) Image-1.jpg , image-1-B.jpg, image-1-A.jpg ?
+# blendedrandom_img = Image.blend(random_file, random_file2, img, .3)
+# blendedrandom_img.save("blended.jpg")
+# # make sure images match up in size 
+# # only takes 3 arguments at a time 
+# # argv 1 being first image argv 2 being second image and third being the opacity 
+# #blending images together 
 
 # Image.open(path.join(currentdirectory,random_file ))
 # Image.open(path.join(currentdirectory,random_file2 ))
@@ -36,7 +62,7 @@ img = Image.open( path.join(sys.argv[1],random_file) )
 # CONVERTING IMAGE SIZE SO THEYRE ALL THE SAME 
 
 
-# if len(sys.argv) != 4:
+# if len(sys.argv) != 3:
 #     #! is to say opposite 
 #    #could change to 4 
 #     #exit("This command requires two arguments")
