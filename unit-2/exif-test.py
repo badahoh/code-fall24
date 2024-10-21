246025ad727eb18549a4aba31bdb75960ef62ec2
badah <ohb234@newschool.edu>
2024-10-21 15:27:41 -0400

    add unit 2

--- unit-2/exif-test.py
+++ unit-2/exif-test.py
@@ -8,3 +8,23 @@ exifData = img.getexif ()
 print(exifData) 
 for key in img.getexif().keys():
     print(key,ExifTags.TAGS[key])
+
+
+# terminal results 
+# {296: 2, 282: 72.0, 34853: 10036, 34665: 360, 271: 'Canon', 272: 'Canon EOS Rebel T6i', 315: 'KARINA', 274: 8, 306: '2024:05:07 12:32:44', 531: 2, 33432: '', 283: 72.0}
+# 34853 GPSInfo
+# 296 ResolutionUnit
+# 34665 ExifOffset
+# 271 Make
+# 272 Model
+# 283 YResolution
+# 274 Orientation
+# 306 DateTime
+# 531 YCbCrPositioning
+# 33432 Copyright
+# 282 XResolution
+# 315 Artist
+
+
+# UNIT 2 
+# for my unit 2 project I want to combine the lyrics of all the songs in one album to automatically write a new song/
