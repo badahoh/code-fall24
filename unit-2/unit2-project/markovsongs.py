ed31858397af11499e00db7633f83a9d674cad83
badah <ohb234@newschool.edu>
2024-11-04 00:34:14 -0500

    unit 2 project

--- unit-2/unit2-project/markovsongs.py
+++ unit-2/unit2-project/markovsongs.py
@@ -1,15 +1,35 @@
+import random 
+import pprint 
 import markovify
 
-text = open("sun.txt").read()
+#text = open("sunceasergambino.txt").read()
+
+#generator = markovify.Text(text)
+
+# with open("sunceasergambino.txt") as f: 
+#     text = f.read()
+
+# generator = markovify.NewLineText(text) 
+
+
+with open("sunceasergambino.txt") as f:
+    text = f.read()
 
 generator = markovify.Text(text)
 
 paragraph = ""
 
-for i in range(10):
-    paragraph += str(generator.make_short_sentence(80))
+for i in range(20):
+    paragraph += str(generator.make_short_sentence(350))
     paragraph += " "
+    word_to_replace = "I'm" "i'm" "i"
+    new_word = "you're" "you're" "you"
+    paragraph = paragraph.replace(word_to_replace, new_word)
 
 print("\n\n\n")
 print(paragraph)
 print("\n\n\n") 
+
+newfile = open("essay2.txt", "w") #write
+newfile.write(paragraph) #taking outcome of generated text and saving it as a txt file
+newfile.close()
